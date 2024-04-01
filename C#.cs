3917f4f3f5a77bf2a6ae6b4c382e3c799f3bec69
bedolaga <tnikita912@gmail.com>
2024-04-01 19:00:03 +0700

    2.cs

---
+++ C#.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> emails = new List<string> { "mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru" };
+        HashSet<string> uniqueEmails = new HashSet<string>();
+
+        foreach (string email in emails)
+        {
+            string cleanedEmail = RemoveDots(email.Substring(0, email.IndexOf('@')));
+            uniqueEmails.Add(cleanedEmail);
+        }
+
+        Console.WriteLine("Count of unique emails: " + uniqueEmails.Count);
+    }
+
+    static string RemoveDots(string s)
+    {
+        string result = "";
+        bool ignore = false;
+        foreach (char c in s)
+        {
+            if (c == '*')
+            {
+                ignore = true;
+            }
+            if (!ignore && c != '.')
+            {
+                result += c;
+            }
+        }
+        return result;
+    }
+}
