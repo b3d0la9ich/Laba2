899a705e2fac4fa9fd7424b10ef2da82367741f1
bedolaga <tnikita912@gmail.com>
2024-04-02 00:13:13 +0700

    7.swift

--- Swift.swift
+++ Swift.swift
@@ -1,33 +1,16 @@
 import Foundation
 
-// Function to remove all dots from a string
-func removeDots(from s: String) -> String {
-    var result = ""
-    var ignore = false // Flag indicating whether to ignore characters
-    for c in s {
-        if c == "*" {
-            ignore = true // Encountered '*', start ignoring
-        }
-        if !ignore && c != "." {
-            result.append(c) // Add character to result if it's not '.' and ignore is not active
+func countEqualNumbers(sequence: [Int]) -> Int {
+    var count = 0
+    for i in 1..<sequence.count {
+        if sequence[i] == sequence[i - 1] {
+            count += 1
         }
     }
-    return result
+    return count
 }
 
-func main() {
-    let emails = ["mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"]
-    var uniqueEmails = Set<String>()
-    
-    // Checking for uniqueness of email addresses after removing dots
-    for email in emails {
-        let localPart = email.components(separatedBy: "@").first ?? ""
-        let cleanedEmail = removeDots(from: localPart)
-        uniqueEmails.insert(cleanedEmail)
-    }
+var sequence = [5, 12, 12, 23, 23, 23, 108]
+let result = countEqualNumbers(sequence: sequence)
+print("Output: \(result)")
 
-    // Output the count of unique emails
-    print("Count of unique emails: \(uniqueEmails.count)")
-}
-
-main()
