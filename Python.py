c8f390a723356d32cd65cb4f68a1ade042b01e5e
bedolaga <tnikita912@gmail.com>
2024-04-02 00:03:58 +0700

    3.py

--- Python.py
+++ Python.py
@@ -1,18 +1,11 @@
-def remove_dots(s):
-    result = ""
-    ignore = False
-    for c in s:
-        if c == '*':
-            ignore = True
-        if not ignore and c != '.':
-            result += c
-    return result
+def count_equal_numbers(sequence):
+    count = 0
+    for i in range(1, len(sequence)):
+        if sequence[i] == sequence[i - 1]:
+            count += 1
+    return count
 
-emails = ["mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"]
-unique_emails = set()
+sequence = [5, 12, 12, 23, 23, 23, 108]
+result = count_equal_numbers(sequence)
+print("Output:", result)
 
-for email in emails:
-    cleaned_email = remove_dots(email[:email.find('@')])
-    unique_emails.add(cleaned_email)
-
-print("Count of unique emails:", len(unique_emails))
