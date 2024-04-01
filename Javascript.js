2eeffaf6eb113e499ceeff5a3135de144cf4844f
bedolaga <tnikita912@gmail.com>
2024-04-02 00:16:22 +0700

    9.js

--- Javascript.js
+++ Javascript.js
@@ -1,27 +1,14 @@
-function removeDots(s) {
-    let result = '';
-    let ignore = false; // Flag indicating whether to ignore characters
-    for (let i = 0; i < s.length; i++) {
-        let c = s.charAt(i);
-        if (c === '*') {
-            ignore = true; // Encountered '*', start ignoring
-        }
-        if (!ignore && c !== '.') {
-            result += c; // Add character to result if it's not '.' and ignore is not active
+function countEqualNumbers(sequence) {
+    let count = 0;
+    for (let i = 1; i < sequence.length; i++) {
+        if (sequence[i] === sequence[i - 1]) {
+            count++;
         }
     }
-    return result;
+    return count;
 }
 
-let emails = ["mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"];
-let uniqueEmails = new Set();
+let sequence = [5, 12, 12, 23, 23, 23, 108];
+let result = countEqualNumbers(sequence);
+console.log("Output: " + result);
 
-// Checking for uniqueness of email addresses after removing dots
-for (let email of emails) {
-    let localPart = email.substring(0, email.indexOf('@'));
-    let cleanedEmail = removeDots(localPart);
-    uniqueEmails.add(cleanedEmail);
-}
-
-// Output the count of unique emails
-console.log("Count of unique emails: " + uniqueEmails.size);
