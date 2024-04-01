481de68d54add1665cd7e4ebe331f47b8fbfbf7a
bedolaga <tnikita912@gmail.com>
2024-04-02 00:11:50 +0700

    6.java

--- Java.java
+++ Java.java
@@ -1,37 +1,28 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
-    public static String removeDots(String s) {
-        String result = "";
-        boolean ignore = false;
-        for (char c : s.toCharArray()) {
-            if (c == '*') {
-                ignore = true;
-            }
-            if (!ignore && c != '.') {
-                result += c;
+    public static int countEqualNumbers(List<Integer> sequence) {
+        int count = 0;
+        for (int i = 1; i < sequence.size(); i++) {
+            if (sequence.get(i).equals(sequence.get(i - 1))) {
+                count++;
             }
         }
-        return result;
+        return count;
     }
 
     public static void main(String[] args) {
-        List<String> emails = new ArrayList<>();
-        emails.add("mar.pha+science@co.rp.nstu.ru");
-        emails.add("marpha+scie.nce@corp.nstu.ru");
-        emails.add("marph.a+s.c.i.e.n.c.e+@corp.nstu.ru");
-
-        Set<String> uniqueEmails = new HashSet<>();
-
-        for (String email : emails) {
-            String cleanedEmail = removeDots(email.substring(0, email.indexOf('@')));
-            uniqueEmails.add(cleanedEmail);
-        }
-
-        System.out.println("Count of unique emails: " + uniqueEmails.size());
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(5);
+        sequence.add(12);
+        sequence.add(12);
+        sequence.add(23);
+        sequence.add(23);
+        sequence.add(23);
+        sequence.add(108);
+        int result = countEqualNumbers(sequence);
+        System.out.println("Output: " + result);
     }
 }
 
