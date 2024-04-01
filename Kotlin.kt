cb95ca5338c3db12ea39a7a54e53937b27988f7a
bedolaga <tnikita912@gmail.com>
2024-04-02 00:14:45 +0700

    8.kt

--- Kotlin.kt
+++ Kotlin.kt
@@ -1,28 +1,18 @@
-fun removeDots(s: String): String {
-    val result = StringBuilder()
-    var ignore = false // Flag indicating whether to ignore characters
-    for (c in s) {
-        if (c == '*') {
-            ignore = true // Encountered '*', start ignoring
-        }
-        if (!ignore && c != '.') {
-            result.append(c) // Add character to result if it's not '.' and ignore is not active
+import kotlin.collections.ArrayList
+
+fun countEqualNumbers(sequence: ArrayList<Int>): Int {
+    var count = 0
+    for (i in 1 until sequence.size) {
+        if (sequence[i] == sequence[i - 1]) {
+            count++
         }
     }
-    return result.toString()
+    return count
 }
 
 fun main() {
-    val emails = arrayOf("mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru")
-    val uniqueEmails = mutableSetOf<String>()
-
-    // Checking for uniqueness of email addresses after removing dots
-    for (email in emails) {
-        val localPart = email.substringBefore('@')
-        val cleanedEmail = removeDots(localPart)
-        uniqueEmails.add(cleanedEmail)
-    }
-
-    // Output the count of unique emails
-    println("Count of unique emails: ${uniqueEmails.size}")
+    val sequence = arrayListOf(5, 12, 12, 23, 23, 23, 108)
+    val result = countEqualNumbers(sequence)
+    println("Output: $result")
 }
+
