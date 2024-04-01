76924e6ad5c8622f9c4f96903b4a2842c329dc41
bedolaga <tnikita912@gmail.com>
2024-04-02 00:09:54 +0700

    5.rb

--- Ruby.rb
+++ Ruby.rb
@@ -1,26 +1,14 @@
-require 'set'
-
-def remove_dots(s)
-  result = ""
-  ignore = false
-  s.each_char do |c|
-    if c == '*'
-      ignore = true
-    end
-    if !ignore && c != '.'
-      result += c
-    end
+def count_equal_numbers(sequence)
+  count = 0
+  (1...sequence.length).each do |i|
+      if sequence[i] == sequence[i - 1]
+          count += 1
+      end
   end
-  result
+  count
 end
 
-emails = ["mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"]
-unique_emails = Set.new
+sequence = [5, 12, 12, 23, 23, 23, 108]
+result = count_equal_numbers(sequence)
+puts "Output: #{result}"
 
-emails.each do |email|
-  local_part = email.split('@').first
-  cleaned_email = remove_dots(local_part)
-  unique_emails.add(cleaned_email)
-end
-
-puts "Count of unique emails: #{unique_emails.size}"
