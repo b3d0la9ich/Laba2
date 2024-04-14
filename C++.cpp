ffd5fc87f8de40e722b7aebe9fc43c52f9b64fa3
bedolaga <tnikita912@gmail.com>
2024-04-15 00:16:24 +0700

    1.cpp

--- C++.cpp
+++ C++.cpp
@@ -5,14 +5,26 @@
 using namespace std;
 
 // Function to remove all dots from a string
-string removeDots(const string& s) {
+string removeDots(const string s) {
     string result;
-    bool ignore = false; // Flag indicating whether to ignore characters
+    for (int i = 0; i < s.size() - 1; i++){
+        if (s[i] == '.' &&  s[i+1] == '.') {
+            return "wrong";
+        }
+        if(s[0] == '.' || s.back() == '.'){
+            return "wrong";
+        }
+    }
+
     for (char c : s) {
         if (c == '*') {
-            ignore = true; // Encountered '*', start ignoring
+            return result;
         }
-        if (!ignore && c != '.') {
+        if (c == '=' || c == '+' || c == '_' || c == '-' || c == ','){
+            return "wrong";
+        }
+        
+        if (c != '.' ) {
             result += c; // Add character to result if it's not '.' and ignore is not active
         }
     }
@@ -21,13 +33,15 @@ string removeDots(const string& s) {
 
 
 int main() {
-    vector<string> emails = { "mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru" };
+    vector<string> emails = { "mar.phas..cience@corp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru" };
     set<string> uniqueEmails;
     
     // Checking for uniqueness of email addresses after removing dots
     for (const string& email : emails) {
         string cleanedEmail = removeDots(email.substr(0, email.find('@')));
+        if (cleanedEmail != "wrong"){
         uniqueEmails.insert(cleanedEmail);
+        }
     }
 
     // Output the count of unique emails
