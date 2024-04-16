614453e60cb2a6f7d228605b39e24c6e62c59d26
bedolaga <tnikita912@gmail.com>
2024-04-16 17:11:56 +0700

    3.go

--- Golang.go
+++ Golang.go
@@ -7,29 +7,62 @@ import (
 
 // Function to remove all dots from a string
 func removeDots(s string) string {
-	var result strings.Builder
-	ignore := false // Flag indicating whether to ignore characters
-	for _, c := range s {
-		if c == '*' {
-			ignore = true // Encountered '*', start ignoring
+	result := ""
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == '.' && s[i+1] == '.' {
+			return "wrong"
 		}
-		if !ignore && c != '.' {
-			result.WriteRune(c) // Add character to result if it's not '.' and ignore is not active
+		if s[0] == '.' || s[len(s)-1] == '.' {
+			return "wrong"
 		}
 	}
-	return result.String()
+
+	for _, c := range s {
+		if c == '*' {
+			return result
+		}
+		if c == '=' || c == '+' || c == '_' || c == '-' || c == ',' || c == '<' || c == '>' || c == '&' || c == '\'' {
+			return "wrong"
+		}
+
+		if c != '.' {
+			result += string(c) // Add character to result if it's not '.' and ignore is not active
+		}
+	}
+	return result
 }
 
 func main() {
-	emails := []string{"mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"}
+	emails := []string{"maaaasdf@dsf@p.nstu.ru", "marphascience@corp.nstu.ru", "marphas.c.i.e.n.c.e@corp.nstu.ru"}
 	uniqueEmails := make(map[string]bool)
+
+	// Проверка длины до символа "@"
+	for _, email := range emails {
+		atPos := strings.Index(email, "@")
+		if atPos != -1 {
+			lengthBeforeAt := atPos
+			if lengthBeforeAt < 6 || lengthBeforeAt > 30 {
+				fmt.Println("Ошибка: количество символов до @ в адресе", email, "должно быть между 6 и 30.")
+				return
+			}
+		} else {
+			fmt.Println("Ошибка: адрес", email, "не содержит символ @.")
+			return
+		}
+	}
 
 	// Checking for uniqueness of email addresses after removing dots
 	for _, email := range emails {
-		atIndex := strings.Index(email, "@")
-		localPart := email[:atIndex]
-		cleanedEmail := removeDots(localPart)
-		uniqueEmails[cleanedEmail] = true
+		// Check if there is more than one '@' in the email
+		if strings.Count(email, "@") > 1 {
+			fmt.Println("Error: More than one '@' symbol in email:", email)
+			return
+		}
+
+		cleanedEmail := removeDots(email[:strings.Index(email, "@")])
+		if cleanedEmail != "wrong" {
+			uniqueEmails[cleanedEmail] = true
+		}
 	}
 
 	// Output the count of unique emails
