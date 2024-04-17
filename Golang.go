a220c69ca2137bd65ed709a12d77b727fcd8444b
bedolaga <tnikita912@gmail.com>
2024-04-17 19:54:06 +0700

    3.go

--- Golang.go
+++ Golang.go
@@ -1,20 +1,47 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+)
 
 func countEqualNumbers(sequence []int) int {
-	count := 0
-	for i := 1; i < len(sequence); i++ {
-		if sequence[i] == sequence[i-1] {
-			count++
-		}
-	}
-	return count
+    count := 0
+    for i := 1; i < len(sequence); i++ {
+        if sequence[i] == sequence[i-1] {
+            count++
+        }
+    }
+    return count
 }
 
 func main() {
-	sequence := []int{5, 12, 12, 23, 23, 23, 108}
-	result := countEqualNumbers(sequence)
-	fmt.Println("Output:", result)
-}
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter the number of elements: ")
+    n, _ := reader.ReadString('\n')
+    n = strings.TrimSpace(n)
+    numElements, err := strconv.Atoi(n)
+    if err != nil {
+        fmt.Println("Error: Invalid input. Please enter an integer.")
+        return
+    }
 
+    sequence := make([]int, numElements)
+    fmt.Println("Enter the elements: ")
+    for i := 0; i < numElements; i++ {
+        input, _ := reader.ReadString('\n')
+        input = strings.TrimSpace(input)
+        num, err := strconv.Atoi(input)
+        if err != nil {
+            fmt.Println("Error: Invalid input. Please enter integers only.")
+            return
+        }
+        sequence[i] = num
+    }
+
+    result := countEqualNumbers(sequence)
+    fmt.Println("Output:", result)
+}
