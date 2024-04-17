ea4165f80eea11edc0797a9c1af5100f0068ca11
bedolaga <tnikita912@gmail.com>
2024-04-17 19:58:08 +0700

    8.rb

--- Ruby.rb
+++ Ruby.rb
@@ -1,14 +1,31 @@
 def count_equal_numbers(sequence)
   count = 0
   (1...sequence.length).each do |i|
-      if sequence[i] == sequence[i - 1]
-          count += 1
-      end
+    if sequence[i] == sequence[i - 1]
+      count += 1
+    end
   end
   count
 end
 
-sequence = [5, 12, 12, 23, 23, 23, 108]
+puts "Enter the number of elements: "
+n = gets.chomp.to_i
+
+if n == 0
+  puts "Error: Invalid input. Please enter an integer."
+  return
+end
+
+sequence = []
+puts "Enter the elements: "
+n.times do
+  element = gets.chomp.to_i
+  if element == 0
+    puts "Error: Invalid input. Please enter integers only."
+    return
+  end
+  sequence << element
+end
+
 result = count_equal_numbers(sequence)
 puts "Output: #{result}"
-
