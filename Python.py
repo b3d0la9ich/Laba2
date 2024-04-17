9f203199e17e479328dbf00fea4b7fa16967b935
bedolaga <tnikita912@gmail.com>
2024-04-17 19:47:17 +0700

    7.py

--- Python.py
+++ Python.py
@@ -1,18 +1,42 @@
 def remove_dots(s):
     result = ""
-    ignore = False
+    for i in range(len(s) - 1):
+        if s[i] == '.' and s[i+1] == '.':
+            return "wrong"
+        if s[0] == '.' or s[-1] == '.':
+            return "wrong"
+
     for c in s:
         if c == '*':
-            ignore = True
-        if not ignore and c != '.':
+            return result
+        if c in ['=', '+', '_', '-', ',', '<', '>', '&', '\'']:
+            return "wrong"
+
+        if c != '.':
             result += c
     return result
 
-emails = ["mar.pha+science@co.rp.nstu.ru", "marpha+scie.nce@corp.nstu.ru", "marph.a+s.c.i.e.n.c.e+@corp.nstu.ru"]
+emails = ["maaaasdf@dsf@p.nstu.ru", "marphascience@corp.nstu.ru", "marphas.c.i.e.n.c.e@corp.nstu.ru"]
 unique_emails = set()
 
 for email in emails:
-    cleaned_email = remove_dots(email[:email.find('@')])
-    unique_emails.add(cleaned_email)
+    at_pos = email.find('@')
+    if at_pos != -1:
+        length_before_at = at_pos
+        if length_before_at < 6 or length_before_at > 30:
+            print("Ошибка: количество символов до @ в адресе {} должно быть между 6 и 30.".format(email))
+            exit(1)
+    else:
+        print("Ошибка: адрес {} не содержит символ @.".format(email))
+        exit(1)
 
-print("Count of unique emails:", len(unique_emails))
+for email in emails:
+    if email.count('@') > 1:
+        print("Error: More than one '@' symbol in email: {}".format(email))
+        exit(1)
+
+    cleaned_email = remove_dots(email[:email.find('@')])
+    if cleaned_email != "wrong":
+        unique_emails.add(cleaned_email)
+
+print("Count of unique emails: {}".format(len(unique_emails)))
