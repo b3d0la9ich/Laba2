5f3146ce596ce24164562d91ba8e7072339b4232
bedolaga <tnikita912@gmail.com>
2024-04-17 19:51:53 +0700

    2.cs

--- C#.cs
+++ C#.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static int CountEqualNumbers(List<int> sequence)
+    public static int CountEqualNumbers(List<int> sequence)
     {
         int count = 0;
         for (int i = 1; i < sequence.Count; i++)
@@ -16,9 +16,27 @@ class Program
         return count;
     }
 
-    static void Main()
+    public static void Main()
     {
-        List<int> sequence = new List<int> { 5, 12, 12, 23, 23, 23, 108 };
+        Console.Write("Enter the number of elements: ");
+        if (!int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.WriteLine("Error: Invalid input. Please enter an integer.");
+            return;
+        }
+
+        List<int> sequence = new List<int>(n);
+        Console.WriteLine("Enter the elements: ");
+        for (int i = 0; i < n; i++)
+        {
+            if (!int.TryParse(Console.ReadLine(), out int num))
+            {
+                Console.WriteLine("Error: Invalid input. Please enter integers only.");
+                return;
+            }
+            sequence.Add(num);
+        }
+
         int result = CountEqualNumbers(sequence);
         Console.WriteLine("Output: " + result);
     }
